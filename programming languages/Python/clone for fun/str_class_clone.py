a496bfd9e2a9533da998dc3ad6a72f8930dfcfe2
hoangharu2002 <nnhoang2002@gmail.com>
2024-10-28 21:34:17 +0700

    optimize upper() method

--- programming languages/Python/clone for fun/str_class_clone.py
+++ programming languages/Python/clone for fun/str_class_clone.py
@@ -3,18 +3,18 @@ class cl_str:
         self.string = init_str
     
     # Clone lại phương thức upper() của lớp str
-    def upper(self):
-        result = ""
+    def cl_upper(self):
+        result = []
         for char in self.string:
             if 'a' <= char <= 'z':
-                result += chr(ord(char) - 32)
+                result.append(chr(ord(char) - 32))
             else:
-                result += char
-        return result
+                result.append(char)
+        return ''.join(result)
 
 
 # Main function
 if __name__ == '__main__':
     s = cl_str('Hello World!')
-    print(s.upper())
+    print(s.cl_upper())
     exit(0)
