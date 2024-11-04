d27cb38d53e6cd299960455d13d90392beca1228
hoangharu2002 <nnhoang2002@gmail.com>
2024-11-04 14:57:03 +0700

    clone lower method and recover missing class __init__

--- programming languages/Python/clone for fun/str_class_clone.py
+++ programming languages/Python/clone for fun/str_class_clone.py
@@ -1,7 +1,8 @@
 class cl_str:
-    def __init__(self, init_str=''):
-        self.string = init_str
-    
+
+    # Phương thức khởi tạo
+    def __init__(self, str_input):
+        self.string = str_input
     # Clone lại phương thức upper() của lớp str
     def cl_upper(self):
         result = []
@@ -11,10 +12,18 @@ class cl_str:
             else:
                 result.append(char)
         return ''.join(result)
-
+    # Clone lại phương thức lower() của lớp str
+    def cl_lower(self):
+        result = []
+        for char in self.string:
+            if 'A' <= char <= 'Z':
+                result.append(chr(ord(char) + 32))
+            else:
+                result.append(char)
+        return ''.join(result)
 
 # Main function
 if __name__ == '__main__':
     s = cl_str('Hello World!')
-    print(s.cl_upper())
+    print(s.cl_lower())
     exit(0)
