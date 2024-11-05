36b992d5ca69f7f0841e886ecd72f4869fa72204
hoangharu2002 <nnhoang2002@gmail.com>
2024-11-05 14:39:34 +0700

    thêm lưu ý lỗi #1

---
+++ programming languages/Python/common error and mistake.py
@@ -0,0 +1,6 @@
+### Những lỗi có thể gặp khi lập trình với python
+
+## 1. Thay đổi giá trị của một đối tượng immutable
+
+s = 'string'    # s (kiểu str) là một đối tượng immutable
+s[2] = 'o'      # Không thể thay đổi giá trị. Báo lỗi "TypeError"
