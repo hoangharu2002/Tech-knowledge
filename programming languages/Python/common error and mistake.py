03a3481c59d5da8557b6c1f5b816c0e0be5ec736
hoangharu2002 <nnhoang2002@gmail.com>
2024-11-25 22:53:29 +0700

    thêm lỗi 2

--- programming languages/Python/common error and mistake.py
+++ programming languages/Python/common error and mistake.py
@@ -4,3 +4,9 @@
 
 s = 'string'    # s (kiểu str) là một đối tượng immutable
 s[2] = 'o'      # Không thể thay đổi giá trị. Báo lỗi "TypeError"
+
+## 2. Thực hiện cộng trừ giữa chuỗi và số
+
+age = 36
+txt = "My name is J, I am " + age
+
